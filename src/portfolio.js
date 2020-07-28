4684b8a9d872781bec6772f0dfbf118ef45e4819
Kamel Benchehida <kamel8007@gmail.com>
2020-07-28 16:54:53 +0100

    []: first commit

---
+++ src/portfolio.js
@@ -0,0 +1,364 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false,  // Change this to false if you don't want Splash screen.
+};
+
+
+//Home Page
+const greeting = {
+	title: "Kamel Benchehida",
+	logo_name: "KamelBenchehida",
+	nickname: "kb8007",
+	subTitle:
+		"A passionate individual who always thrive to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+	resumeLink: "#",
+	portfolio_repository: "https://github.com/kb8007",
+};
+
+const socialMediaLinks = {
+	/* Your Social Media Link */
+	github: "https://github.com/kb8007",
+	linkedin: "https://www.linkedin.com/in/kamel-benchehida/",
+	gmail: "kamel8007@gmail.com",
+	facebook: "https://www.facebook.com/Benchehidakamel/",
+	twitter: "https://twitter.com/benchehidakamel",
+	instagram: "https://www.instagram.com/kamel_benchehida/"
+};
+
+const skills = {
+  data: [
+    {
+      title: "Data Science & AI",
+      fileName: "DataScienceImg",
+      skills: [
+        "⚡ Developing highly scalable production ready models for various deep learning and statistical use cases",
+        "⚡ Experience of working with Computer Vision and NLP projects",
+        "⚡ Complex quantitative modelling for dynamic forecasting and time series analysis"
+      ],
+      softwareSkills: [
+        {
+          skillName: "Tensorflow",
+          fontAwesomeClassname: "logos-tensorflow",
+          style: {
+            backgroundColor: "transparent"
+          }
+        },
+        {
+          skillName: "Keras",
+          fontAwesomeClassname: "simple-icons:keras",
+          style: {
+            backgroundColor: "white",
+            color: "#D00000"
+          }
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "logos-python",
+          style: {
+
+          }
+        }
+      ]
+    },
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+		"⚡ Developing desktop applications using Java and Python",  
+		"⚡ Developing mobile applications using Flutter and solo android apps using Dart",
+		"⚡ Developing on Odoo using Python, JavaScript, XML, HTML5 and CSS3",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "logos-java",
+          style: {
+            
+          }
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B"
+          }
+        },
+		{
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#FF0000"
+          }
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6"
+          }
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E"
+          }
+        }
+      ]
+    },
+    {
+      title: "UI/UX Design",
+      fileName: "DesignImg",
+      skills: [
+        "⚡ Designing highly attractive user interface for mobile, desktop and web applications",
+        "⚡ Creating the flow of application functionalities to optimize user experience"
+      ],
+      softwareSkills: [
+
+      ]
+    }
+  ]
+};
+
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    // {
+    //   siteName: "HackerRank",
+    //   iconifyClassname: "simple-icons:hackerrank",
+    //   style: {
+    //     color: "#2EC866"
+    //   },
+    //   profileLink: "https://www.hackerrank.com/layman_brother"
+    // },
+  ]
+}
+
+const degrees = {
+  degrees : [
+    {
+      title: "University of Sciences and Technology Houari Boumediene",
+      subtitle: "Master in artificial intelligence",
+      logo_path: "usthb_logo.png",
+      alt_name: "USTHB",
+      duration: "2019",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like Algorithms and AI etc.",
+        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
+      ],
+      website_link: "https://www.usthb.dz/fr"
+	},
+	{
+		title: "University of Sciences and Technology Houari Boumediene",
+		subtitle: "License in computer science",
+		logo_path: "usthb_logo.png",
+		alt_name: "USTHB",
+		duration: "2017",
+		descriptions: [
+			""
+		],
+		website_link: "https://www.usthb.dz/fr"
+	  }
+  ]
+}
+
+const certifications = {
+	certifications: [
+		{
+			title: "Neural Networks in Python: Deep Learning",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-e15a2473-8297-4a0a-9fa3-2e9e099ec146/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "C Programming - Master the C Fundamentals",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-e57ea98c-8a2c-472b-b5ea-292bab9a5450/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "Advanced JavaScript",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-b942868b-d557-4111-93f7-47e7b19ed59e/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "Python 3 Guide",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-a483ba72-9f6a-44db-a1ec-cf9902c7c632/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "GUI Programming and CSV in Python: Basics (Project Oriented)",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-aa1a0da5-4f24-4046-9190-9746fd8056ee/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "Advanced SQL Guide",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-22725dd7-2b33-4598-a4f7-06c91ad60ccd/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "Python Basics for Math and Data Science 1.0: Numpy and Sympy",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-d780cd33-c7bd-4452-85b9-410f01ca2575/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "Python Object Oriented Programming",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-84496813-cc0b-4574-91bd-47731e3a7291/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "All the basics of Python",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-7302a495-f8ed-40f3-8b18-1b245d4e261c/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "The HTML5",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-472d4940-fcce-433e-b3c1-d007426c39a6/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "Remote Teaching Online",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-d41d3a17-9412-4342-ad37-1a980e615da5/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+		{
+			title: "The SQL",
+			subtitle: "",
+			logo_path: "udemy_logo.png",
+			certificate_link: "https://www.udemy.com/certificate/UC-0660b657-31f8-479b-95bb-ea529ccb75e5/",
+			alt_name: "",
+			color_code: "#DCDCDC"
+		},
+	]
+};
+
+
+// Experience Page
+const experience = {
+	title: "Experience",
+	subtitle: "Work, Internship and Volunteership",
+	description: "",
+	header_image_path: "experience.svg",
+	sections: [
+		{
+			title: "Work",
+			experiences: [
+				{
+					title: "Odoo developer",
+					company: "Elosys - IGPRO. Ltd.",
+					company_url: "https://www.igpro-online.net",
+					logo_path: "igpro_elosys.png",
+					duration: "January 2020 - Present",
+					location: "Mostaganem, Algeria",
+					description: "",
+					color: "#005026",
+				},
+			],
+		},
+		{
+			title: "Internship",
+			experiences: [
+				{
+					title: "Odoo developer",
+					company: "Elosys - IGPRO. Ltd.",
+					company_url: "https://www.igpro-online.net",
+					logo_path: "igpro_elosys.png",
+					duration: "October 2019 - December 2019",
+					location: "Mostaganem, Algeria",
+					description: "",
+					color: "#005026",
+				},
+			],
+		},
+		{
+			title: "Volunteership",
+			experiences: [
+
+			],
+		},
+	],
+};
+
+// Projects Page
+const projectsHeader = {
+	title: "Projects",
+	description:
+		"My projects makes use of vast variety of latest technology tools. My best experience is to create Many projects in different fields (Data science, Desktop, Web, Mobile, etc.).",
+	avatar_image_path: "projects_image.svg",
+};
+
+
+// Contact Page
+const contactPageData = {
+	contactSection: {
+		title: "Contact Me",
+		profile_image_path: "my_profile_pic.png",
+		description:
+			"I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, Dev mobile (Android/IOS), Odoo and Opensource Development.",
+	},
+	blogSection: {
+		title: "Youtube",
+		subtitle:
+			"Courses about Algorithmic and Machine structure",
+		link: "https://www.youtube.com/KamelBenchehida",
+		avatar_image_path: "blogs_image.svg",
+	},
+	addressSection: {
+		title: "Address",
+		subtitle: "52 rue mustapha ben boulaid, Sirat, Mostaganem, Algeria - 27021",
+		avatar_image_path: "address_image.svg",
+		location_map_link: "https://www.google.com/maps/place/Sirat/@35.7752586,0.1431301,12z/data=!4m13!1m7!3m6!1s0x1281cf170aa6c033:0x5593842c8c33d50b!2sMascara+Province!3b1!8m2!3d35.3904125!4d0.1494988!3m4!1s0x1281f1a69cdbb489:0xb18c414c470a5bb4!8m2!3d35.7794377!4d0.189836",
+	},
+	phoneSection: {
+		title: "Phone Number",
+		subtitle: "+213 775927045",
+	},
+};
+
+export {
+	settings,
+	greeting,
+	socialMediaLinks,
+	skills,
+	competitiveSites,
+	degrees,
+	certifications,
+	experience,
+	projectsHeader,
+	contactPageData,
+};
